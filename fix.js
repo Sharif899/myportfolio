c5cfb7e6a9e6e72065aa34e513fa9b9a001782a5
Sharif <sherifolabode@gmail.com>
2026-05-21 22:09:59 +0100

    Fix CRLF line endings in all source files

---
+++ fix.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+let content = fs.readFileSync('src/components/sections/Contact.tsx', 'utf8');
+content = content.replace(/\r\n/g, '\n');
+content = content.replace(/\n{3,}/g, '\n\n');
+fs.writeFileSync('src/components/sections/Contact.tsx', content, 'utf8');
+console.log('Done');
---
+++ fixall.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('fs');
+
+function fixFile(filePath) {
+  const content = fs.readFileSync(filePath, 'utf8');
+  if (content.includes('\r\n')) {
+    const fixed = content.replace(/\r\n/g, '\n');
+    fs.writeFileSync(filePath, fixed, 'utf8');
+    console.log('Fixed:', filePath);
+  }
+}
+
+function walkDir(dir) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const fullPath = dir + '/' + file;
+    const stat = fs.statSync(fullPath);
+    if (stat.isDirectory() && file !== 'node_modules' && file !== '.next') {
+      walkDir(fullPath);
+    } else if (file.endsWith('.ts') || file.endsWith('.tsx')) {
+      fixFile(fullPath);
+    }
+  }
+}
+
+walkDir('./src');
+console.log('All done');
